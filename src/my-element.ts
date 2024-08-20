bd93afc108d56e10932ce4d77d980dab5f970daf
Diana Broeders <diana.broeders@sanoma.com>
2024-08-20 16:45:16 +0200

    load multiple icons

--- src/my-element.ts
+++ src/my-element.ts
@@ -1,7 +1,7 @@
 import { LitElement, css, html, nothing } from 'lit'
 import { customElement, property } from 'lit/decorators.js'
 import { setup } from '@sl-design-system/sanoma-learning';
-import { faCat, faDog } from '@fortawesome/free-solid-svg-icons';
+import { faCat, faDog, faFish, faHippo, faHorse, faKiwiBird, faMosquito, faOtter, faShrimp, faSpider, faWorm } from '@fortawesome/free-solid-svg-icons';
 import { Icon } from '@sl-design-system/icon';
 import { ifDefined } from 'lit/directives/if-defined.js';
 
@@ -21,6 +21,7 @@ export class MyElement extends LitElement {
   count = 0
 
   @property() loadedOnClick?: string;
+  @property({type: Boolean}) zooLoaded?: boolean;
 
   override connectedCallback(): void {
     super.connectedCallback();
@@ -30,21 +31,44 @@ export class MyElement extends LitElement {
 
   render() {
     return html`
-       <sl-button @click=${this._onClick} part="button">
-          Load additional icons
-        </sl-button>
+       
         <table>
-          <tr><td>from included collection</td><td><sl-icon name="info"></sl-icon></td></tr>
-          <tr><td>registered on component load (connectedCallback)</td><td><sl-icon name="fas-cat"></sl-icon></td></tr>
-          <tr><td>registered on button click</td><td>${this.loadedOnClick?html`<sl-icon .name=${this.loadedOnClick}></sl-icon>`:nothing}</td></tr>
+          <tr><td>from included collection</td><td><sl-icon name="info"></sl-icon></td><td></td></tr>
+          <tr><td>registered on component load (connectedCallback)</td><td><sl-icon name="fas-cat"></sl-icon></td><td></td></tr>
+          <tr>
+            <td>single icon registered on button click</td>
+            <td><sl-button @click=${this.#loadSingle} part="button">Load dog</sl-button></td>
+            <td>${this.loadedOnClick?html`<sl-icon .name=${this.loadedOnClick}></sl-icon>`:nothing}</td>
+          </tr>
+          <tr>
+            <td>whole zoo registered on button click</td>
+            <td><sl-button @click=${this.#loadMultiple} part="button">Load zoo</sl-button></td>
+            <td>${this.zooLoaded?html`
+              <sl-icon name="fas-hippo"></sl-icon>
+              <sl-icon name="fas-fish"></sl-icon>
+              <sl-icon name="fas-otter"></sl-icon>
+              <sl-icon name="fas-kiwi-bird"></sl-icon>
+              <sl-icon name="fas-worm"></sl-icon>
+              <sl-icon name="fas-spider"></sl-icon>
+              <sl-icon name="fas-shrimp"></sl-icon>
+              <sl-icon name="fas-mosquito"></sl-icon>
+              <sl-icon name="fas-horse"></sl-icon>
+              `:nothing}</td>
+          </tr>
         </table> 
     `
   }
 
-  private _onClick() {
+  private #loadSingle() {
 
     Icon.register(faDog);
     this.loadedOnClick = 'fas-dog';
+  }
+
+  private #loadMultiple() {
+
+    Icon.register(faHippo, faFish, faOtter, faKiwiBird, faWorm, faSpider, faShrimp, faMosquito, faHorse);
+    this.zooLoaded = true;
   }
 
   static styles = css`
